acc765d400630610044ffb1d8cee671e9cf202be
Samuel.Lin <samuel.lin@SamuelLin-mac.local>
2022-08-12 09:32:28 +0800

    Lowest Common Ancestor of a Binary Search Tree

---
+++ Lowest Common Ancestor of a Binary Search Tree/index.js
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+  while (root) {
+    if (p.val > root.val && q.val > root.val) {
+      root = root.right;
+    } else if (p.val < root.val && q.val < root.val) {
+      root = root.left;
+    } else {
+      return root;
+    }
+  }
+};
